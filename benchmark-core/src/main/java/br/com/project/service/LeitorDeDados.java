aee151bf1610b05e5890fccdd1a5e26162699bc9
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2025-03-14 02:02:20 -0300

    implementa resultados e graficos

--- benchmark-core/src/main/java/br/com/project/service/LeitorDeDados.java
+++ benchmark-core/src/main/java/br/com/project/service/LeitorDeDados.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.nio.file.Paths;
 
 /*
- * Classe que lê o arquivo de dados.json e mapeia o objeto
+ * Classe que lê o arquivo de dados e mapeia o objeto
  */
 public class LeitorDeDados {
 
@@ -17,13 +17,14 @@ public class LeitorDeDados {
      *
      * @param classReturn Classe do objeto a ser retornado.
      * @param <T> Tipo genérico do objeto.
+     * @param fileName nome do arquivo de dados.
      * @return Objeto do tipo T com os dados do JSON.
      * @throws IOException Se houver erro na leitura do arquivo.
      */
-    public static <T> T readJson(Class<T> classReturn) throws IOException {
+    public static <T> T readJson(Class<T> classReturn, String fileName) throws IOException {
         String currentDir = Paths.get("").toAbsolutePath().toString();
         System.err.println(currentDir);
-        File filePath = new File(currentDir + "/benchmark-core/src/main/java/br/com/project/data/dados.json");
+        File filePath = new File(currentDir + "/benchmark-core/src/main/java/br/com/project/data/" + fileName);
 
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
